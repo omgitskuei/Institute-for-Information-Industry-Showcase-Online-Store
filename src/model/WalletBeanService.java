1d597108dd63d15462aa1c1434f8580656966d33
Kuei-Feng Tung <kueifengtungchris@gmail.com>
2020-01-21 14:24:35 +0800

    WalletBeanService v2 (fin)

--- src/model/WalletBeanService.java
+++ src/model/WalletBeanService.java
@@ -1,7 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Currency;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import util.CheckSubstring;
 
 @Service
 public class WalletBeanService {
@@ -18,25 +23,43 @@ public class WalletBeanService {
 	// Validity means checking user input for proper $ money values
 	public boolean updateAmount(WalletBean updateThisWallet) {
 		System.out.println("BEGIN: WalletBeanService.insert(WalletBean thisWallet)");
+		// Local variables
 		boolean success = false;
-		
-		
-		if ( validateMoney() ) {
+		// Validate Money before updating amount
+		if ( validateMoney(updateThisWallet.getWalletAmount()) ) {
 			wDAO.updateWalletAmount(updateThisWallet, updateThisWallet.getWalletAmount());
 		}
-		
 		System.out.println("FINISH: WalletBeanService.insert(WalletBean thisWallet)");
 		return success;
 	}
 	
 	// Private Validation Methods
-	private static boolean validateMoney() {
+	// Money values that make sense only come in 2 demical points
+	private static boolean validateMoney(float thisMoney) {
 		boolean success = false;
 		try {
+			System.out.println("BEGIN: WalletBeanService.validateMoney(float)");
+			// Get an instance of the local Currency
+			Currency localCurrency = Currency.getInstance("TWD");
+			// Get how many decimals are allowed for this Currency
+			int validCurrencyDecimals = localCurrency.getDefaultFractionDigits();  //equals =2 for TWD
+			// Check if the user input has at most this amount of decimals
+			String thisMoneyString = Float.toString(thisMoney);
+			// Get the length of the substring after the "."
+			CheckSubstring util = new CheckSubstring();
+			ArrayList<String> delimitedThisMoney = util.delimitAtDot(thisMoneyString);
+			// If user entered a number with <= the number of digits allowed, success = TRUE
+			if (validCurrencyDecimals <= delimitedThisMoney.get(1).length()) {
+				System.out.println("Wallet Amount has valid num of decimals");
+				System.out.println("FINISH: WalletBeanService.validateMoney(float)");
+				success = true;
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("ERROR: INVALID MONEY AMOUNT");
+		System.out.println("FINISH: WalletBeanService.validateMoney(float)");
 		return success;
 	}
 }
