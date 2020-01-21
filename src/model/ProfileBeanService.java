4d7f6219371fca5dc50820ab9fbbf09bb922e196
studentmkp <carring456@gmail.com>
2020-01-21 17:26:20 +0800

    update ProfileBeanService, have some problems about joinDateTime and vip check

--- src/model/ProfileBeanService.java
+++ src/model/ProfileBeanService.java
@@ -1,5 +1,103 @@
 package model;
 
-public class ProfileBeanService {
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import util.CheckSubstring;
+
+@Service
+public class ProfileBeanService {
+	// Varibles: Local Fields
+	private ProfileBeanDAO pDAO;
+
+	// Constructors
+	@Autowired
+	public ProfileBeanService(ProfileBeanDAO pDAO) {
+		this.pDAO = pDAO;
+	}
+	
+	// Test validity of ProfileBean user input
+	// Validity means making sure values make sense
+	public boolean  insert(ProfileBean newProfile) {
+		System.out.println("BEGIN: ProfileBeanService.insert(ProfileBean newProfile)");
+		// Get values from newProfile for validation
+		String fullName = newProfile.getProfileFullName();
+		Date birthday = newProfile.getProfileBirthdate();
+		String sex = newProfile.getProfileSex();
+		String phone = newProfile.getProfilePhone();
+		String address = newProfile.getProfileAddress();
+		int userVIP = newProfile.getProfileVIP();
+		
+		// TODO JOIN DATE LocalDateTime.now(), there is a type problemg
+		
+		boolean success = false;
+		// Validate values
+		// not yet check : joinDate, birthday, userVIP
+		if (validateFullName(fullName) && validateSex(sex) && validatePhone(phone) && validateAddress(address)) {
+			pDAO.insertProfile(newProfile);
+		}
+		
+		System.out.println("FINISH: ProfileBeanService.insert(ProfileBean newProfile)");
+		return success;
+	}
+	
+	private static boolean validateFullName(String fullName) {
+		boolean valid = false;
+		try {
+			CheckSubstring util = new CheckSubstring();
+			int countSpec = util.countSpecialCharacters(fullName);
+			if(countSpec >1) {
+				
+			}
+		} catch (Exception e) {
+
+		}
+		return valid;
+	}
+	
+	private static boolean validateSex(String sex) {
+		boolean valid = false;
+		try {
+			CheckSubstring util = new CheckSubstring();
+			int countSpec = util.countSpecialCharacters(sex);
+			if(countSpec >1) {
+				
+			}
+		} catch (Exception e) {
+
+		}
+		return valid;
+	}
+	
+	private static boolean validatePhone(String phone) {
+		boolean valid = false;
+		try {
+			CheckSubstring util = new CheckSubstring();
+			int countSpec = util.countSpecialCharacters(phone);
+			if(countSpec >1) {
+				
+			}
+		} catch (Exception e) {
+
+		}
+		return valid;
+	}
+	
+	private static boolean validateAddress(String address) {
+		boolean valid = false;
+		try {
+			CheckSubstring util = new CheckSubstring();
+			int countSpec = util.countSpecialCharacters(address);
+			if(countSpec >1) {
+				
+			}
+		} catch (Exception e) {
+
+		}
+		return valid;
+	}
+	
 }
