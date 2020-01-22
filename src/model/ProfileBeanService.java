2a9804fa4c088a0b9bbd0cdc1d06fd2ec0ad5073
Chienlin <luchienlin1994@gmail.com>
2020-01-22 10:13:25 +0800

    Update ProfileBeanService.java

--- src/model/ProfileBeanService.java
+++ src/model/ProfileBeanService.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,7 @@ public class ProfileBeanService {
 	public ProfileBeanService(ProfileBeanDAO pDAO) {
 		this.pDAO = pDAO;
 	}
-	
+
 	// Test validity of ProfileBean user input
 	// Validity means making sure values make sense
 	public boolean insert(ProfileBean newProfile) {
@@ -28,40 +27,42 @@ public class ProfileBeanService {
 		String sex = newProfile.getProfileSex();
 		String phone = newProfile.getProfilePhone();
 		String address = newProfile.getProfileAddress();
-		
-		// TODO JOIN DATE LocalDateTime.now(), there is a type problemg
+
+		// TODO JOIN DATE, there is a type problem
 		Date birthDate = newProfile.getProfileBirthdate();
-		
-		
+
 		boolean success = false;
 		// Validate values
-		// not yet check : joinDate, birthday, userVIP
-		if (validateFullName(fullName) && validateSex(sex) && validatePhone(phone) && validateAddress(address) && validateBirthdate(birthdate)) {
+		if (validateFullName(fullName) && validateSex(sex) && validatePhone(phone) && validateAddress(address)
+				&& validBirthdate(birthDate)) {
 			pDAO.insertProfile(newProfile);
 		}
-		
+
 		System.out.println("FINISH: ProfileBeanService.insert(ProfileBean newProfile)");
 		return success;
 	}
-	
-	public boolean updateBirthdate(ProfileBean thisBean) {
+
+	public boolean validBirthdate(Date birthDate) {
 		boolean valid = false;
-		Date birthdate = thisBean.getProfileBirthdate();
 		
-		if (v) {
-			pDAO.updateBirthdate(thisBean, birthdate);
-		} else {
-			;
+		try {
+			if (birthDate != null) {
+				valid = true;
+			} else {
+				valid = false;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return valid;
 	}
-	
+
 	private static boolean validateFullName(String fullName) {
 		boolean valid = false;
 		try {
 			CheckSubstring util = new CheckSubstring();
 			int countSpec = util.countSpecialCharacters(fullName);
-			if(countSpec==0) {
+			if (countSpec == 0) {
 				valid = true;
 			}
 		} catch (Exception e) {
@@ -69,14 +70,14 @@ public class ProfileBeanService {
 		}
 		return valid;
 	}
-	
+
 	private static boolean validateSex(String sex) {
 		boolean valid = false;
 		try {
 			CheckSubstring util = new CheckSubstring();
 			int countSpec = util.countSpecialCharacters(sex);
 			// Sex is a letter "m", "f", etc. No Special characters, only 1 letter
-			if(countSpec==0 && sex.length()==1) {
+			if (countSpec == 0 && sex.length() == 1) {
 				valid = true;
 			}
 		} catch (Exception e) {
@@ -84,35 +85,33 @@ public class ProfileBeanService {
 		}
 		return valid;
 	}
-	
+
 	private static boolean validatePhone(String phone) {
 		boolean valid = false;
 		try {
 			// checks to make sure only numbers have been entered
 			CheckSubstring util = new CheckSubstring();
-			if(util.countNums(phone)==phone.length()) {
+			if (util.countNums(phone) == phone.length()) {
 				valid = true;
 			}
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 		return valid;
 	}
-	
+
 	private static boolean validateAddress(String address) {
 		boolean valid = false;
 		try {
 			CheckSubstring util = new CheckSubstring();
 			int countSpec = util.countSpecialCharacters(address);
-			if(countSpec>1) {
+			if (countSpec > 1) {
 				valid = true;
 			}
 		} catch (Exception e) {
-
+			e.printStackTrace();
 		}
 		return valid;
 	}
-	
-	private static boolean validateBirth
-	
+
 }
