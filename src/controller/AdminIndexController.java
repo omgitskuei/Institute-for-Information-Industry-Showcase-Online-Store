ecaee56540cff7be0b1044f8362e74a993fedcfd
Chienlin <luchienlin1994@gmail.com>
2020-01-30 12:03:59 +0800

    AdminIndex

--- src/controller/AdminIndexController.java
+++ src/controller/AdminIndexController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -22,4 +21,7 @@ public class AdminIndexController {
 		System.out.println("Directing to AdminLogin");
 		return "AdminLogin";
 	}
+
+
+
 }
