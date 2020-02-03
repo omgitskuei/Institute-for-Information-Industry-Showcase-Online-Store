5cd1553192912111c11927b0c7f70e694e26443a
alexslashland <58679571+alexslashland@users.noreply.github.com>
2020-02-03 16:52:59 +0800

    FindUniqueProductController v1.0.1

--- src/controller/FindUniqueProductController.java
+++ src/controller/FindUniqueProductController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -15,7 +16,8 @@ public class FindUniqueProductController {
 	
 	@RequestMapping(path="/controller.FindUniqueProductController", method=RequestMethod.GET)
 	public String processAction(@SessionAttribute("productCategory")String ProductCategory, Model nextPage) {
-		nextPage.getAttribute(ProductName);
+		nextPage.getAttribute("ProductName", productName);
+		System.out.println("Directing to Product");
 		return "UniqueProduct";
 	}
 }
