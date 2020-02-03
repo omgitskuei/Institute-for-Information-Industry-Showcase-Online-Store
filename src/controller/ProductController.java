b1943ec10bdd0e042206765b3de88cafa97747a4
mingjuang <57438734+mingjuang@users.noreply.github.com>
2020-02-03 13:56:45 +0800

    product controller 0203

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -2,12 +2,13 @@ package controller;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,151 +16,133 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-import model.Picture;
-import model.PictureService;
+import model.Account;
+import model.product.ProductBean;
+import model.product.ProductBeanService;
 
-public class ProductController extends AbstractController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+
+
+@Controller
+@SessionAttributes(names = {"name", "price","stock","description","timestamp","category", "errors"})
+public class ProductController {
 	
-		String name = request.getParameter("name");
-		Map<String, String> map = new HashMap<String, String>();
-		if (name == null || name.length() == 0) {
-			map.put("name", "name is required");
-		}
-		request.setAttribute("errors", map);
-		if (map != null && !(map.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		String price = request.getParameter("price");
-		Map<String, String> pricemap = new HashMap<String, String>();
-		if (price == null || price.length() == 0) {
-			pricemap.put("pricee", "price is required");
-		}
-		request.setAttribute("errors", pricemap);
-		if (pricemap != null && !(pricemap.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		String stock = request.getParameter("stock");
-		Map<String, String> stockmap = new HashMap<String, String>();
-		if (stock == null || stock.length() == 0) {
-			stockmap.put("stock", "stock is required");
-		}
-		request.setAttribute("errors", stockmap);
-		if (stockmap != null && !(stockmap.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		String description = request.getParameter("description");
-		Map<String, String> descriptionmap = new HashMap<String, String>();
-		if (description == null || description.length() == 0) {
-			stockmap.put("description", "description is required");
-		}
-		request.setAttribute("errors", descriptionmap);
-		if (descriptionmap != null && !(descriptionmap.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		String timestamp = request.getParameter("timestamp");
-		Map<String, String> timestampmap = new HashMap<String, String>();
-		if (timestamp == null || timestamp.length() == 0) {
-			timestampmap.put("timestamp", "timestamp is required");
-		}
-		request.setAttribute("errors", timestampmap);
-		if (timestampmap != null && !(timestampmap.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		String category = request.getParameter("category");
-		Map<String, String> categorymap = new HashMap<String, String>();
-		if (category == null || category.length() == 0) {
-			categorymap.put("category", "category is required");
-		}
-		request.setAttribute("errors", timestampmap);
-		if (categorymap != null && !(categorymap.isEmpty())) {
-			return new ModelAndView("form"); 
-		}
-		
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("name", name);
-		session.setAttribute("price", price);
-		session.setAttribute("stock", stock);
-		session.setAttribute("description", description);
-		session.setAttribute("timestamp", timestamp);
-		session.setAttribute("category", category);
-		
-		
-		return new ModelAndView("success"); 	
-		
-	}
-		
 	@Autowired
 	private HttpServletRequest request;
-	private PictureService pService;
+	private ProductBeanService prservice;
+
 	
-	@Autowired
-	public ProductController(PictureService pService) {
-		this.pService = pService;
-	}
-
-		@RequestMapping(path = "/Productcontroller", method = RequestMethod.POST)
-		@ResponseBody
-		public ResponseEntity<byte[]> uploadFile(@RequestParam(name = "Image") MultipartFile multipartfile)
-				throws Exception {
-			String fileName = multipartfile.getOriginalFilename();
-			System.out.println("fileName=" + fileName);
-
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.IMAGE_JPEG);
-
-			String savePath = request.getSession().getServletContext().getRealPath("/") + "uploadTempDir\\" + fileName;
-			System.out.println("savePath=" + savePath);
-
-			File saveFile = new File(savePath);
-			multipartfile.transferTo(saveFile);
-
-			if (fileName != null && fileName.length() != 0) {
-				savePicture(fileName, savePath);
-			}
-
-			return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(saveFile), headers, HttpStatus.OK);
-		}
-
+	@RequestMapping(path="/productbean.controller", method = RequestMethod.POST)
+	public String processAction(@RequestParam(name = "name") String name, 
+			                    @RequestParam(name = "price") float price,
+			                    @RequestParam(name = "stock") int stock,
+			                    @RequestParam(name = "description") String description,
+			                    @RequestParam(name = "timestamp") Date timestamp,
+			                    @RequestParam(name = "category") String category,
+			                    Model pr) throws ParseException {
+	
 		
-		private void savePicture(String fileName, String savePath) {
-			try {
-				Picture picture = new Picture();
-				picture.setFilename(fileName);
-
-				FileInputStream is1 = new FileInputStream(savePath);
-				byte[] data = new byte[is1.available()];
-				is1.read(data);
-				is1.close();
-
-				picture.setPicture(data);
-				pService.insert(picture);			
-			} catch (Exception e) {
-	            e.printStackTrace();
-			}
+		Map<String, String> errors = new HashMap<String, String>();
+		pr.addAttribute("errors",errors);
+		
+		if (name == null || name.length() == 0) {
+			errors.put("name", "name is required");
+		}	
+		String s =String.valueOf(price);
+		if (s == null|| s.length() == 0 ) {
+			errors.put("price", "price is required");
 		}
+		String st =String.valueOf(stock);
+		if ( st == null || st.length() == 0) {
+			errors.put("stock", "stock is required");
+		}
+		
+		if (description == null || description.length() == 0) {
+			errors.put("description", "description is required");
+		}
+		SimpleDateFormat sdf =  new SimpleDateFormat("yyyy-MM-dd");
+		String ti =sdf.format(timestamp);
+		Date date = sdf.parse(ti);
+		if (ti == null || ti.length() == 0) {
+			errors.put("timestamp", "timestamp is required");
+		}
+		
+		if (category == null || category.length() == 0) {
+			errors.put("category", "category is required");
+		}
+		
+		if (errors!=null && !errors.isEmpty()) {
+			return "productbean";
+		}
+		
+		
+		pr.addAttribute("name", name);
+		pr.addAttribute("price", price);
+		pr.addAttribute("stock", stock);
+		pr.addAttribute("description", description);
+		pr.addAttribute("timestamp", date);
+		pr.addAttribute("category", category);
+		
+			return "product";
+		}		
+		
+	
+	
+		
+	
+
+//	@Autowired
+//	public ProductControlle(ProductBeanService service) {
+//		this.prservice = prservice;
+//	}
+//		
+//		@ResponseBody
+//		public ResponseEntity<byte[]> uploadFile(@RequestParam(name = "Image") MultipartFile multipartfile)
+//				throws Exception {
+//			String Img = multipartfile.getOriginalFilename();
+//			System.out.println("Img=" + Img);
+//
+//			HttpHeaders headers = new HttpHeaders();
+//			headers.setContentType(MediaType.IMAGE_JPEG);
+//
+//			String savePath = request.getSession().getServletContext().getRealPath("/") + "uploadTempDir\\" + Img;
+//			System.out.println("savePath=" + savePath);
+//
+//			File saveFile = new File(savePath);
+//			multipartfile.transferTo(saveFile);
+//
+//			if (Img != null && Img.length() != 0) {
+//				savePicture(Img, savePath);
+//			}
+//
+//			return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(saveFile), headers, HttpStatus.OK);
+//		}
+//	
+//		private void savePicture(byte[] Img, String savePath) {
+//			try {
+//				ProductBean Image = new ProductBean();
+//				Image.setProductImg(Img);;
+//
+//				FileInputStream is1 = new FileInputStream(savePath);
+//				byte[] data = new byte[is1.available()];
+//				is1.read(data);
+//				is1.close();
+//
+//				Image.setProductImg(data);
+//				prservice.insert(Img);			
+//			} catch (Exception e) {
+//	            e.printStackTrace();
+//			}
+//		}
 				
 }
 	
