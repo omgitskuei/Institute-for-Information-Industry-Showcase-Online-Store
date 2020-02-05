03a3ebdb2d9579f7d0d3e0fe87a82085e63c5b3e
Jerry <carring456@gmail.com>
2020-02-05 20:22:50 +0800

    fix transactional error

--- src/model/profile/ProfileBeanService.java
+++ src/model/profile/ProfileBeanService.java
@@ -3,36 +3,33 @@ package model.profile;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-public class ProficeService implements ProfileBeanServiceInterface {
+@Service
+public class ProfileBeanService implements ProfileBeanServiceInterface {
 	
 	@Autowired
 	private ProfileBeanDAO profileDAO;
 	
 	@Override
-	@Transactional
 	public List<ProfileBean> getProfiles() {
-		// TODO Auto-generated method stub
 		return profileDAO.getProfiles();
 	}
 
 	@Override
-	@Transactional
 	public void saveProfile(ProfileBean theProfile) {
 		profileDAO.saveProfile(theProfile);
 	}
 
 	@Override
-	@Transactional
-	public ProfileBean getProfile(int userId) {
-		return profileDAO.getProfile(userId);
+	public ProfileBean getProfile(int userID) {
+		return profileDAO.getProfile(userID);
 	}
 
 	@Override
-	@Transactional
-	public void deleteProfile(int userId) {
-		profileDAO.deleteProfile(userId);
+	public void deleteProfile(int userID) {
+		profileDAO.deleteProfile(userID);
 	}
 
 }
