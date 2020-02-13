f3ba721d47a215bc63d5fade848f79a1d0aac3f6
Kuei-Feng Tung <kueifengtungchris@gmail.com>
2020-02-13 16:15:50 +0800

    EmailUsers +sendWelcomeEmail (fin)

--- src/util/EmailUsers.java
+++ src/util/EmailUsers.java
@@ -7,35 +7,41 @@ import org.apache.commons.mail.SimpleEmail;
 
 public class EmailUsers {
 	// Local fields
-	
+	private String fromEmail = "farmvilletaiwan@gmail.com";
+	private String fromEmailPwd = "Qq12345!";
+	private String fromEmailHostName = "smtp.googlemail.com";
+	private int fromEmailPort = 465;	
 	
 	// Constructors
-	
+	public EmailUsers() {
+		System.out.println("BEGIN: util.EmailUsers()");
+	}
 	
 	// Executable
 	public static void main(String[] args) {
 		EmailUsers thisClass = new EmailUsers();
-		// thisClass.sendWelcomeEmail("farmvilletaiwan@gmail.com", "Bob");
+//		thisClass.sendWelcomeEmail(thisClass.toEmail, "Bob");
+//		thisClass.sendChangePwdEmail("farmvilletaiwan@gmail.com", "Joe");
+//		thisClass.sendReceiptEmail("farmvilletaiwan@gmail.com", "Bob", 122031, "2020/2/18", "1200.00", "F3, #12, 292 Minsheng Rd, Taipei, Taiwan");
 	}
 	
 	// Methods
 	public void sendChangePwdEmail(String userEmail, String userName) {
 		// String userName can be a user's Display name, Email, or Full name
-		System.out.println("BEGIN: util.changePwdEmail");
+		System.out.println("BEGIN: util.EmailUsers.sendChangePwdEmail(String, String)");
 		// Local Variables
-		String fromEmail = "farmvilletaiwan@gmail.com";
 		String toEmail = userEmail;
 		try {
 			// Instantiate SimpleEmail object
 			Email email = new SimpleEmail();
 			// Email sending settings
-			email.setHostName("smtp.googlemail.com");
-			email.setSmtpPort(465);
+			email.setHostName(this.fromEmailHostName);
+			email.setSmtpPort(this.fromEmailPort);
 			// Email sender's credentials
-			email.setAuthenticator(new DefaultAuthenticator("farmvilletaiwan@gmail.com", "Qq12345!"));
+			email.setAuthenticator(new DefaultAuthenticator(this.fromEmail, this.fromEmailPwd));
 			email.setSSLOnConnect(true);
 			// Email TO / FROM
-			email.setFrom(fromEmail);
+			email.setFrom(this.fromEmail);
 			email.addTo(toEmail);
 			// Email body
 			email.setSubject("FarmVille Account Support: Your password changed");
@@ -54,51 +60,62 @@ public class EmailUsers {
 					+ "The FarmVille Support Team");
 			// Send
 			email.send();
-			System.out.println("Email to ["+toEmail+"] from ["+fromEmail+"] sent!");
+			System.out.println("Email to ["+toEmail+"] from ["+this.fromEmail+"] sent!");
 		} catch (EmailException e) {
 			e.printStackTrace();
 			System.out.println("ERROR: TestUtil Email Exception");
 		}
-		System.out.println("FINISH: util.changePwdEmail");
+		System.out.println("FINISH: util.EmailUsers.sendChangePwdEmail(String, String)");
 	}
 	
-	public void sendReceiptEmail(String userEmail, String userName) {
+	public void sendReceiptEmail(String userEmail, String userName, int orderID, String orderTimestamp, String orderTotal, String profileAddress) {
 		// String userName can be a user's Display name, Email, or Full name
-		System.out.println("BEGIN: util.EmailUsers.sendReceiptEmail");
+		System.out.println("BEGIN: util.EmailUsers.sendReceiptEmail(String, String, int, String, String, String");
 		// Local Variables
 		String fromEmail = "farmvilletaiwan@gmail.com";
 		String toEmail = userEmail;
-//		"Hi "+userName+","+" \r\n" 
-//		+ " \r\n" 
-//		+ "Thank you for your recent transaction on Farmville, Taiwan. "+"\r\n"
-//		+ " \r\n" 
-//		+ "If you didn’t change your password, contact our support email right away at farmvilletaiwan@gmail.com"+"\r\n"
-//		+ " \r\n" 
-//		+ "Just a reminder:"+"\r\n"
-//		+ "- Never share your password or security questions with anyone."+"\r\n"
-//		+ "- Create passwords that are hard to guess and don’t use personal information. Be sure to include uppercase and lowercase letters, numbers, and symbols."+"\r\n"
-//		+ "- Use different passwords for each of your online accounts."+"\r\n"
-//		+ " \r\n" 
-//		+ "Thanks,"+"\r\n"
-//		+ "The FarmVille Support Team");
-		String bodyPart1 = "";
-		String bodyPart2 = "";
-		String bodyPart3 = "";
+
+		String bodyPart1 = "Hi "+userName+",\r\n" + 
+				"\r\n" + 
+				"Thank you for your recent transaction on Farmville, Taiwan.\r\n" + 
+				"This email message will serve as your receipt.\r\n" + 
+				"You can also view your Purchase History at any time.\r\n" + 
+				"\r\n";
+		String bodyPart2 = "Receipt from Farmville, Taiwan\r\n" + 
+				"Receipt #:\r\n" + 
+				orderID+"\r\n" + 
+				"\r\n" + 
+				"Account name:\r\n" + 
+				userEmail+"\r\n" + 
+				"\r\n" + 
+				"Date issued:\r\n" + 
+				orderTimestamp+"\r\n" + 
+				"\r\n" + 
+				"Total:\r\n" + 
+				"NT$ "+orderTotal+"\r\n" + 
+				"\r\n" + 
+				"Billing address:\r\n" + 
+				profileAddress+"\r\n" + 
+				"\r\n";
+		String bodyPart3 = "If you have any questions, contact us at farmvilletaiwan@gmail.com.\r\n" + 
+				"\r\n" + 
+				"Thanks,\r\n" + 
+				"The FarmVille Support Team";
 		String emailBody = bodyPart1+bodyPart2+bodyPart3;
 		try {
 			// Instantiate SimpleEmail object
 			Email email = new SimpleEmail();
 			// Email sending settings
-			email.setHostName("smtp.googlemail.com");
-			email.setSmtpPort(465);
+			email.setHostName(this.fromEmailHostName);
+			email.setSmtpPort(this.fromEmailPort);
 			// Email sender's credentials
-			email.setAuthenticator(new DefaultAuthenticator("farmvilletaiwan@gmail.com", "Qq12345!"));
+			email.setAuthenticator(new DefaultAuthenticator(this.fromEmail, this.fromEmailPwd));
 			email.setSSLOnConnect(true);
 			// Email TO / FROM
-			email.setFrom(fromEmail);
+			email.setFrom(this.fromEmail);
 			email.addTo(toEmail);
 			// Email body
-			email.setSubject("FarmVille Account Support: Your password changed");
+			email.setSubject("Your FarmVille receipt [#"+orderID+"]");
 			email.setMsg(emailBody);
 			// Send
 			email.send();
