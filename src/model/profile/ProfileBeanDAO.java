32dfdb625347d8b31d3046dc85e3e5b061466123
studentmkp <carring456@gmail.com>
2020-02-26 20:10:42 +0800

    use HQL to select user in ProfileBeanDAO

--- src/model/profile/ProfileBeanDAO.java
+++ src/model/profile/ProfileBeanDAO.java
@@ -65,14 +65,21 @@ public class ProfileBeanDAO implements ProfileBeanDAOInterface {
 	@Override
 	public ProfileBean getProfile(int userID) {
 		Session currentSession = sessionFactory.getCurrentSession();
-		ProfileBean theProfile = currentSession.get(ProfileBean.class, userID);
+//		ProfileBean theProfile = currentSession.get(ProfileBean.class, userID);
+		String hqlQ = "From ProfileBean where userID=:userID";
+		Query query = currentSession.createQuery(hqlQ);
+		query.setParameter("userID", userID);
+		ProfileBean theProfile = (ProfileBean) query.uniqueResult();
 		return theProfile;
 	}
 	
 	public ProfileBean getProfile(ProfileBean thisP) {
 		Session currentSession = sessionFactory.getCurrentSession();
 		int userID = thisP.getUserID();
-		ProfileBean theProfile = currentSession.get(ProfileBean.class, userID);
+		String hqlQ = "From ProfileBean where userID=:userID";
+		Query query = currentSession.createQuery(hqlQ);
+		query.setParameter("userID", userID);
+		ProfileBean theProfile = (ProfileBean) query.uniqueResult();
 		return theProfile;
 	}
 	
@@ -94,12 +101,13 @@ public class ProfileBeanDAO implements ProfileBeanDAOInterface {
 		return results;
 	}
 
-	@Override
-	public void deleteProfile(int userID) {
-		Session session = sessionFactory.getCurrentSession();
-		ProfileBean theProfile = session.byId(ProfileBean.class).load(userID);
-		session.delete(theProfile);
-
-	}
+//  沒有要刪除使用者
+//	@Override
+//	public void deleteProfile(int userID) {
+//		Session session = sessionFactory.getCurrentSession();
+//		String hqlQ = "delete ProfileBean where userID=:userID";
+//		Query query = session.createQuery(hqlQ);
+//		query.setParameter("userID", userID);
+//	}
 
 }
