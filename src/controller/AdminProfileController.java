481447e892fa71dcde7cec26c01513b7b866a802
Jerry <carring456@gmail.com>
2020-03-09 14:43:12 +0800

    Admin show the user setting

--- src/controller/AdminProfileController.java
+++ src/controller/AdminProfileController.java
@@ -25,6 +25,8 @@ import model.orderDetails.OrderDetailsBeanDAO;
 import model.profile.ProfileBean;
 import model.profile.ProfileBeanDAO;
 import model.profile.ProfileBeanService;
+import model.setting.SettingBean;
+import model.setting.SettingBeanService;
 import model.user.UserBean;
 import model.user.UserBeanService;
 import model.wallet.WalletBean;
@@ -56,6 +58,9 @@ public class AdminProfileController {
 	
 	@Autowired
 	private OrderDetailsBeanDAO orderDetailsDAO;
+	
+	@Autowired
+	private SettingBeanService settingService;
 
 	
 	// 列出所有使用者頁面
@@ -207,6 +212,35 @@ public class AdminProfileController {
 
 		return "OrderDetailsList";
 	}
+	
+	@GetMapping("/showTheUserSetting")
+	public String showTheUserSetting(@RequestParam(value = "userID") int userID, Model m) {
+		SettingBean theUserSetting = settingService.select(userID);
+		System.out.println("Setting ID is " + theUserSetting.getSettingID());
+		System.out.println("User ID is " + theUserSetting.getUserID());
+		System.out.println("Security Q is " + theUserSetting.getSettingSecurityQ());
+		System.out.println("Security A is " + theUserSetting.getSettingSecurityA());
+		System.out.println("Allow metadata is " + theUserSetting.getSettingAllowMetadata());
+		
+		m.addAttribute("userSetting",theUserSetting);
+		System.out.println("UserSetting is :" + theUserSetting);
+		return "AdminShowTheUserSetting";
+	}
+	
+//	@PostMapping("/updateSetting")
+//	public String updateSetting(@ModelAttribute SettingBean updateThisSetting,
+//			                  @RequestParam(value="settingSecurityQ", required=false) String newQ,
+//			                  @RequestParam(value="settingSecurityA", required=false) String newA,
+//			                  @RequestParam(value="settingDisplayName", required=true) String newSettingDisplayName,
+//			                  @RequestParam(value="settingAllowMetadata", required=true) boolean newSettingAllowMetadata) {
+//		
+//		settingDAO.updateSettingSecurityQ(updateThisSetting, newQ);
+//		settingDAO.updateSettingSecurityA(updateThisSetting, newA);
+//		settingDAO.updateSettingDisplayName(updateThisSetting, newSettingDisplayName);
+//		settingDAO.updateSettingAllowMetadata(updateThisSetting, newSettingAllowMetadata);
+//		
+//		return "redirect:/UserProfile/showTheUserSetting";
+//	}
 	
 	
 
